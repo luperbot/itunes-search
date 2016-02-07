8d678eeb513c2ea79b96e27169c3f9a0270da0e7
Claudia Mane <c@luperlabs.com>
2016-02-07 01:35:20 -0800

    Added basic fields for AppDetails model data.

--- models.py
+++ models.py
@@ -1,5 +1,9 @@
+from datetime import datetime
+from time import strptime, mktime
+
 import requests
 from flywheel import Model, Field, Engine
+from flywheel.fields import types
 
 # Testing configuration values
 DB_REGION = 'us-west-1'
@@ -8,6 +12,77 @@ DB_PORT = 8000
 DB_KEY = 'dummy'
 DB_SECRET = 'dummy'
 DB_SECURE = False
+
+STORE_LOOKUP_URL = 'https://itunes.apple.com/lookup'
+
+
+class AppDetails(Model):
+    app_id = Field(data_type=types.IntType, hash_key=True, coerce=True)
+    country = Field(data_type=types.StringType)
+    advisories = Field(data_type=types.ListType)
+    artistId = Field(data_type=types.IntType, coerce=True)
+    artistName = Field(data_type=types.StringType)
+    artistViewUrl = Field(data_type=types.StringType)
+    artworkUrl100 = Field(data_type=types.StringType)
+    artworkUrl512 = Field(data_type=types.StringType)
+    artworkUrl60 = Field(data_type=types.StringType)
+    averageUserRating = Field(data_type=types.FloatType, coerce=True)
+    averageUserRatingForCurrentVersion = Field(
+            data_type=types.FloatType,
+            coerce=True
+    )
+    bundleId = Field(data_type=types.StringType)
+    contentAdvisoryRating = Field(data_type=types.StringType)
+    currency = Field(data_type=types.StringType)
+    currentVersionReleaseDate = Field(data_type=types.DateTimeType)
+    description = Field(data_type=types.StringType)
+    features = Field(data_type=types.ListType)
+    fileSizeBytes = Field(data_type=types.IntType, coerce=True)
+    formattedPrice = Field(data_type=types.StringType)
+    genreIds = Field(data_type=types.ListType)
+    genres = Field(data_type=types.ListType)
+    ipadScreenshotUrls = Field(data_type=types.ListType)
+    isGameCenterEnabled = Field(data_type=types.BoolType)
+    isVppDeviceBasedLicensingEnabled = Field(data_type=types.BoolType)
+    kind = Field(data_type=types.StringType)
+    languageCodesISO2A = Field(data_type=types.ListType)
+    minimumOsVersion = Field(data_type=types.StringType)
+    price = Field(data_type=types.FloatType, coerce=True)
+    primaryGenreId = Field(data_type=types.IntType, coerce=True)
+    primaryGenreName = Field(data_type=types.StringType)
+    releaseDate = Field(data_type=types.DateTimeType, coerce=True)
+    releaseNotes = Field(data_type=types.StringType)
+    screenshotUrls = Field(data_type=types.ListType)
+    sellerName = Field(data_type=types.StringType)
+    sellerUrl = Field(data_type=types.StringType)
+    supportedDevices = Field(data_type=types.ListType)
+    trackCensoredName = Field(data_type=types.StringType)
+    trackContentRating = Field(data_type=types.StringType)
+    trackId = Field(data_type=types.IntType, coerce=True)
+    trackName = Field(data_type=types.StringType)
+    trackViewUrl = Field(data_type=types.StringType)
+    userRatingCount = Field(data_type=types.IntType, coerce=True)
+    userRatingCountForCurrentVersion = Field(
+            data_type=types.IntType,
+            coerce=True
+    )
+    version = Field(data_type=types.StringType)
+    wrapperType = Field(data_type=types.StringType)
+
+    def __init__(self, app_id, country, data=None):
+        if data is None:
+            data = {}
+
+        self.app_id = app_id
+        self.country = country
+        for field, value in data.items():
+            if 'Date' in field:
+                value = strptime(value, "%Y-%m-%dT%H:%M:%SZ")
+                value = datetime.fromtimestamp(mktime(value))
+            setattr(self, field, value)
+
+    def save(self, overwrite=True):
+        engine.save(self, overwrite=overwrite)
 
 
 def setup_dynamodb(models, region=DB_REGION, access_key=DB_KEY,
@@ -45,4 +120,27 @@ def setup_dynamodb(models, region=DB_REGION, access_key=DB_KEY,
     return engine
 
 
-engine = setup_dynamodb([])
+def load_app_details(app_ids, countries):
+    for app_id in app_ids:
+        for country in countries:
+            data = get_app_data(app_id, country)
+            app_details = AppDetails(app_id, country, data=data)
+            app_details.save()
+
+
+def get_app_data(app_id, country):
+    params = {'id': app_id, 'country': country}
+    response = requests.get(STORE_LOOKUP_URL, params=params)
+    data = response.json()['results']
+    if not data:
+        return
+    return data[0]
+
+
+engine = setup_dynamodb([AppDetails])
+
+
+if __name__ == '__main__':
+    app_ids = [726232588, 328412701, 651510680]
+    countries = ['us', 'mx', 'jp', 'kr', 'ru', 'fr', 'de']
+    load_app_details(app_ids, countries)
